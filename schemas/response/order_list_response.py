1c9cb9379677129df133c0bda9267666f51f3cfa
HeegyoungChoi <heegyoung.choi@switchwon.com>
2023-05-03 15:54:05 -0400

    remove not used import

--- schemas/response/order_list_response.py
+++ schemas/response/order_list_response.py
@@ -1,14 +1,8 @@
 from datetime import datetime
-from decimal import Decimal
-from typing import Optional
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel
 
-from config.settings import settings
-from services.crypto.cipher import decrypt_aes_cbc
 from services.enums.order_status import OrderStatus
-from services.enums.user import AccountLevel
-from services.validator.helper import is_phone
 
 
 class OrderListResponse(BaseModel):
