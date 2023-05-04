d77b47e7a8f420cb82c733234ffb55659182532b
HeegyoungChoi <gyowhk.dev@gmail.com>
2023-05-04 11:04:07 -0400

    apply pre commit

--- models/event.py
+++ models/event.py
@@ -12,16 +12,8 @@ class Event(Model, TimeStampMixin):
     end_date: datetime = fields.DatetimeField()
     title: str = fields.CharField(max_length=50)
     is_display: BooleanField = fields.BooleanField()
-    page_url: str = fields.CharField( max_length=2048, null=True)
-    end_page_url: str = fields.CharField(
-        max_length=2048, null=True
-    )
-    banner_image_url: str = fields.CharField(
-        max_length=2048, null=True
-    )
-    list_image_url: str = fields.CharField(
-        max_length=2048, null=True
-    )
-    is_delete: BooleanField = fields.BooleanField(
-        default=True, null=True
-    )
+    page_url: str = fields.CharField(max_length=2048, null=True)
+    end_page_url: str = fields.CharField(max_length=2048, null=True)
+    banner_image_url: str = fields.CharField(max_length=2048, null=True)
+    list_image_url: str = fields.CharField(max_length=2048, null=True)
+    is_delete: BooleanField = fields.BooleanField(default=True, null=True)
